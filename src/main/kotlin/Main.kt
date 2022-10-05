aa1b39fe3f4ee7879f61c4625caf5f41dd3059bc
Darren <20088411@mail.wit.ie>
2022-10-05 16:16:21 +0100

    Add Logging capabilities to function calls

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
+import mu.KotlinLogging
+
 import java.lang.System.exit
 
 import utils.ScannerInput
 
+private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
     runMenu()
 }
@@ -37,19 +40,19 @@ fun runMenu() {
 }
 
 fun addNote(){
-    println("You chose Add Note")
+    logger.info { "addNote() function invoked" }
 }
 
 fun listNotes(){
-    println("You chose List Notes")
+    logger.info { "listNotes() function invoked" }
 }
 
 fun updateNote(){
-    println("You chose Update Note")
+    logger.info { "updateNote() function invoked" }
 }
 
 fun deleteNote(){
-    println("You chose Delete Note")
+    logger.info { "deleteNote() function invoked" }
 }
 
 fun exitApp(){
